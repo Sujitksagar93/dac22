808c13d14fc317539f095b62c12beae0a7b09351
Sujitksagar93 <114222158+Sujitksagar93@users.noreply.github.com>
2022-10-22 08:54:43 +0530

    Delete Nilesh_Demos/Doubts directory

--- Nilesh_Demos/Doubts/BankTest.java
+++
@@ -1,9 +0,0 @@
-class BankTest{
-		public static void main(String[] args){
-			bank.Finance bn = new bank.Finance();
-			double interest = bn.interest(10000, 10, 3);
-
-			System.out.printf("Calculated Interest is %f%n", interest);
-
-		}
-}
--- Nilesh_Demos/Doubts/Finance.java
+++
@@ -1,9 +0,0 @@
-package bank;
-//default, public, private
-public class Finance{
-    //private, public, protected, default
-	public static double interest(double a, double r, int n){
-
-		return (a * r * n) / 100;
-	}
-}
